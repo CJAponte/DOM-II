15b6064952694fe5623eea630889276dcba0899b
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-05-14 16:52:46 -0400

    completed MVP. added 10 unique event listeners

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const navBarFunBus = document.querySelector(".main-navigation .logo-heading")
 const contentIMGs = document.getElementsByClassName("img-content")
 const contentSection = document.querySelectorAll(".content-section p")
 const navItems = document.querySelectorAll(".nav a")
+const bottomIMG = document.querySelector(".content-destination img")
+const bottomButtons = document.getElementsByClassName("btn")
 
 
 headerIMGBus.addEventListener("mouseenter",()=>{
@@ -43,4 +45,37 @@ window.addEventListener("scroll", ()=>{
     })
 })
 
-console.log(contentSection)
+window.addEventListener("load", ()=>{
+    alert("Welcome to the Fun Bus Website!")
+})
+
+contentIMGs[0].addEventListener("drag", ()=>{
+    contentIMGs[0].style.opacity = ".5"
+})
+
+contentIMGs[1].addEventListener("drag", ()=>{
+    contentIMGs[1].style.opacity = ".15"
+})
+
+bottomIMG.addEventListener("contextmenu", ()=>{
+    contentIMGs[0].style.display = "none"
+    contentIMGs[1].style.display = "none"
+    headerIMGBus.style.display = "none"
+    bottomIMG.style.display = "none"
+    navBarFunBus.style.display = "none"
+    event.preventDefault();
+})
+
+
+bottomButtons[0].addEventListener("mouseover", ()=>{
+    bottomButtons[0].style.background = "orange"
+})
+
+bottomButtons[1].addEventListener("mouseover", ()=>{
+    bottomButtons[1].style.background = "orange"
+})
+
+bottomButtons[2].addEventListener("mouseover", ()=>{
+
+    bottomButtons[2].style.background = "orange"
+})
