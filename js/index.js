60a2b7d51a2313d7894d678997b8a969da099907
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-05-14 14:07:34 -0400

    Have added 6 events to my page. Trying to figure out more creative ways of modifying the webpage.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 // Your code goes here
+
+
+const headerIMGBus = document.querySelector(".intro img")
+const navBarFunBus = document.querySelector(".main-navigation .logo-heading")
+const contentIMGs = document.getElementsByClassName("img-content")
+const contentSection = document.querySelectorAll(".content-section p")
+const navItems = document.querySelectorAll(".nav a")
+
+
+headerIMGBus.addEventListener("mouseenter",()=>{
+    headerIMGBus.style.display = "none"
+});
+
+
+navBarFunBus.addEventListener("mouseleave", ()=>{
+    headerIMGBus.style.display = "flex"
+});
+
+contentIMGs[0].addEventListener("dblclick", ()=>{
+        contentIMGs[0].style.visibility = "hidden"
+});
+
+contentIMGs[1].addEventListener("dblclick", ()=>{
+    contentIMGs[1].style.visibility = "hidden"
+});
+
+navBarFunBus.addEventListener("click", ()=>{
+    contentIMGs[0].style.visibility = "visible"
+    contentIMGs[1].style.visibility = "visible"
+});
+
+
+window.addEventListener("resize",()=>{
+    navItems.forEach((el)=>{
+        el.style.color = "green"
+    })
+})
+
+window.addEventListener("scroll", ()=>{
+    contentSection.forEach((el)=>{
+        el.style.color = "blue"
+    })
+})
+
+console.log(contentSection)
